ec9552fdb2a82e191ce3ae1916449ca26a4a0145
IND333D <75726574+IND333D@users.noreply.github.com>
2021-01-19 23:04:17 +0530

    First
    Ball breaker first commit

---
+++ Ball breaker Multiplayer/Assets/Ball.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    Vector2 ballvel;
+    Vector3 currentvel;
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        ballvel = new Vector2(4, -4);
+        rb.velocity = ballvel;
+    }
+
+    private void Update()
+    {
+        // calculating current velocity.
+        // speed limit.
+        currentvel = rb.velocity;
+        if(currentvel.y > 6)
+        {
+            rb.velocity = new Vector2(currentvel.x, 6);
+        }
+        if (currentvel.y < -6)
+        {
+            rb.velocity = new Vector2(currentvel.x, -6);
+        }
+        if (currentvel.x > 6)
+        {
+            rb.velocity = new Vector2(6,currentvel.y);
+        }
+        if (currentvel.y < -6)
+        {
+            rb.velocity = new Vector2(-6, currentvel.y);
+        }
+
+        //Adding force when velocity in a particular direction becomes less or 0.
+        // preventing it from momving only vertical or horizontal
+        if (rb.velocity.y < 2 && rb.velocity.y > -2)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 3f);
+        }
+        if (rb.velocity.x < 2 && rb.velocity.x > -2)
+        {
+            rb.velocity = new Vector2(3f, rb.velocity.x);
+        }
+    }
+}
---
+++ Ball breaker Multiplayer/Assets/MovePad.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePad : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.position = new Vector3(transform.position.x + horizontal * 0.2f,transform.position.y,0);
+    }
+}
